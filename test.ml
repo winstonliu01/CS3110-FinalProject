8a8d2a0e438720ad4224c51ebe229d780a987667
winstonliu01 <65269141+winstonliu01@users.noreply.github.com>
2021-03-26 18:59:38 -0400

    Delete test.ml

--- test.ml
+++
@@ -1,9 +0,0 @@
-open OUnit2
-
-(* Remember to open up each file independently*)
-(* In case we need set-like lists, we can use helper functions*)
-
-(*Insert test names here*)
-let suite = "Test suite for Blackjack Game" >::: List.flatten []
-
-let _ = run_test_tt_main suite
