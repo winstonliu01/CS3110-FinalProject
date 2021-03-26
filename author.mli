534b658fdef7bded332c9c702fcddc75ab7b4bd6
winstonliu01 <65269141+winstonliu01@users.noreply.github.com>
2021-03-26 19:44:38 -0400

    Delete author.mli

--- author.mli
+++
@@ -1,9 +0,0 @@
-(** CS 3110 Spring 2021 Final Project
-
-    @author Winston Liu (wdl33)
-    @author Jerry Peng (jzp5)
-    @author Lyon "JJ" Li (lbl53) *)
-
-
-(** [hours_worked] is the number of hours each member worked on this assignment. *)
-val hours_worked : int list
