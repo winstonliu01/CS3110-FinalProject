9479f19eb3f6e218b14429a4b5aaeb60a8261a0e
Winston Liu <wdl33@cornell.edu>
2021-03-26 19:29:44 -0400

    Created author

--- author.ml
+++ author.ml
@@ -1,2 +1,2 @@
-(* TODO: set the value below, then delete this TODO comment. *)
-let hours_worked = -1
+
+let hours_worked = [0 ; 0; 0]
--- author.mli
+++ author.mli
@@ -1,42 +1,9 @@
-(************************************************************ 
-   Copyright (C) 2021 Cornell University.
-   Created by Michael Clarkson (mrc26@cornell.edu) and CS 3110 course staff.
-   You may not redistribute this assignment, distribute any derivatives,
-   or use it for commercial purposes.
- ************************************************************)
+(** CS 3110 Spring 2021 Final Project
 
-(* TODO: change the name and NetID below to your own, then delete this
-   TODO comment. *)
+    @author Winston Liu (wdl33)
+    @author Jerry Peng (jzp5)
+    @author Lyon "JJ" Li (lbl53) *)
 
-(** CS 3110 Spring 2021 Assignment A2 Adventure
 
-    @author Your Name Here (your netid here) *)
-
-(* TODO: complete the academic integrity statement below, then delete
-   this TODO comment. *)
-
-(************************************************************ 
-
-   Academic Integrity Statement
-
-   I, the person named in the @author comment above, have fully reviewed the
-   course academic integrity policies.  I have adhered to those policies in
-   solving the assignment.
-
-   The policies do permit some limited collaboration among students currently
-   enrolled in the course. If I did engage in such collaboration, here is the
-   list of other students with whom I collaborated, and a brief summary of that
-   collaboration:
-
-   - none
-
-   Here are any deviations from the policies that I want to document, with the
-   understanding that being honest about my mistakes is more honorable than
-   lying about my intellectual work:
-
-   - none
-
- ************************************************************)
-
-(** [hours_worked] is the number of hours you worked on this assignment. *)
-val hours_worked : int
+(** [hours_worked] is the number of hours each member worked on this assignment. *)
+val hours_worked : int list
