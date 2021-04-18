a10f03e6b4c58483bbe044af5c4c29b77e8b5574
Winston Liu <wdl33@cornell.edu>
2021-04-17 20:13:49 -0400

    Added .mli specs

--- player.mli
+++ player.mli
@@ -7,15 +7,21 @@ type player = {
   chips : int;
 }
 
+(*type representing a dealer *)
 type dealer = {
   hand : card list;
   hand_val : int;
 }
 
+(*[point_add] is the value after the current card has been applied to
+  the total*)
 val point_add : int -> card -> int
 
+(*[ace_checker] is the value of the ace depending on the scenario*)
 val ace_checker : int -> int
 
+(*[player_init] is the state of the player when the game begins*)
 val player_init : player
 
+(*[dealer_init] is the state of the dealer when the game begins*)
 val dealer_init : dealer
--- round.mli
+++ round.mli
@@ -2,14 +2,20 @@ open Text
 open Deck
 open Player
 
+(*[print_hand] prints out the cards someone has in their hand*)
 val print_hand : card list -> unit
 
+(*[card] prints out the first card in the deck*)
 val card : deck -> unit
 
+(*[parse_input] prints depending on whether a hit or stay is inputted*)
 val parse_input : deck -> int -> unit
 
+(*[command] prompts the user for an action*)
 val command : deck -> int -> unit
 
+(*[start_round] is the state of the game when a new round begins*)
 val start_round : deck -> player -> dealer -> unit
 
+(*[init_deck] is the deck when the game begins*)
 val init_deck : deck
